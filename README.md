bf7db2f89889cf6eba2f0afe7dbc2cf92bf9dfac
dipeshpandya <dipesh.pandya@gmail.com>
2024-05-29 18:57:18 -0400

    uploaded

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# customer_banking
-Creating a customer banking system that allows users to calculate and track interest earned on savings and CD accounts.
+#python-challenge-1
+# Module 3 Challenge
+
+## Customer banking
+
+### Description
+The application is a cstomer facing backing system desgined to let customer calculate and track interested earned on savings and CD accounts. The application is designed to use concepts of object oriented programming by calling methods and modules from a different file.
+
+### Features
+* User is prompted to select an account type to know how much interest will be earned for an account. User is promoted to enter in uppercase though application is designed for user to enter selection in either case.
+* User is promted to give their inputs on balance, interest rate and time horizon to calculate interest
+* Validation is done for each prompt to ensure user has entered an integer, If user inadvertantly enters a non integer charatcer, application display the erros and loops the user back to account selection. Though this was not a requirement of the challenge, I considered it a good step considering calcuations are involved
+* Once all information is correctly entered, screen displays 
+    * Interest calculated
+    * Total balance with interest at the end of time horizon
+* Application loops back the user to account type selection prompt
+* The prompt shows selecting Q to (Q)uit the application. User can also enter any other key to quit the application.   
+
+### How does it work
+* The application covers (4) files
+    1. Account.py
+    2. customer_banking.py
+    3. savings_account.py
+    4. cd_account.py
+
+* Application is executed from customer_banking.py.
+* Prompts entered by user are used to call respective functions for savings account or CD account.
+* The methods are located in their individual files. Each method is imported into customer_banking.py
+* The calculations are carried out as part of the functions by passing the parameters to the function.
+* In each of the account files, Account funtion is imported to be able to update interest and updated principal amount to respective accounts.
+
+## Cencepts used
+1. Importing functions
+2. Passing variables to an external function
+3. Match/Case
+4. While loops
---
+++ customer_banking.py
@@ -0,0 +1,54 @@
+# Import the create_cd_account and create_savings_account functions
+# ADD YOUR CODE HERE
+from cd_account import create_cd_account
+from savings_account import create_savings_account
+
+# Define the main function
+def main():
+    """This function prompts the user to enter the savings and cd account balance, interest rate,
+    and the length of months to determine the interest gained.
+    It displays the interest earned on the savings and CD accounts and updates the balances.
+    """
+    # Prompting the user to select type of account to calculate interest earned     
+
+    while True:
+        calculate_balance = input("Calculate balance for (C)D accout, (S)avings account or (Q)uit ").upper()
+        if calculate_balance == 'C':
+            account_type = "CD account"
+        elif calculate_balance == 'S':
+            account_type = "savings account"
+        else:
+            False
+            break
+
+    # Prompt the user to set the balance, interest rate, and months for the selected account type.
+    # ADD YOUR CODE HERE
+    
+        account_balance = int(input(f"Please enter {account_type} account balance? "))
+        account_interest = int(input(f"Enter annual interest rate on {account_type} ? "))
+        account_maturity = int(input("Please enter time period in years to calculate intereste earned ? "))
+        account_maturity_months = account_maturity * 12
+
+
+        match(calculate_balance):
+#        if calculate_balance == 'S':
+            case 'S':
+            # Call the create_savings_account function and pass the variables from the user.
+                updated_balance, interest_earned = create_savings_account(round(account_balance,2), round(account_interest,2), account_maturity_months)
+
+            case 'C':
+            # Call the create_cd_account function and pass the variables from the user.             
+                updated_balance, interest_earned = create_cd_account(round(account_balance,2), round(account_interest,2), account_maturity_months)
+ 
+        # Print out the interest earned and updated CD account balance with interest earned for the given months.
+        # ADD YOUR CODE HERE
+
+        print(f"On a {account_type} with ${account_balance}, earning {account_interest}% per year, interest earned after {account_maturity} will be ${interest_earned}")
+        print(f"Updated balance for {account_type} at the end of {account_maturity} years will be ${updated_balance}")
+
+# call main() function
+
+if __name__ == "__main__":
+    main()
+
+
